f4eb6322df6791bb5a1d7a13d0542e914b195b10
territeri <dennisstartsev@gmail.com>
2025-02-10 11:29:01 +0300

    Create ParserStatus.tsx

---
+++ components/ParserStatus.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ParserStatus: React.FC = () => {
+  return (
+    <div>
+      <p>Status: Running</p>
+      <p>Messages Parsed: 1000</p>
+      <p>Last Update: 2 minutes ago</p>
+    </div>
+  )
+}
+
+export default ParserStatus

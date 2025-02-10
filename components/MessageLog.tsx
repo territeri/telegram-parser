aa8ce3c8d324780437c495bd03f8ad4b54210781
territeri <dennisstartsev@gmail.com>
2025-02-10 11:29:26 +0300

    Create MessageLog.tsx

---
+++ components/MessageLog.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MessageLog: React.FC = () => {
+  return (
+    <div>
+      <ul>
+        <li>Message 1</li>
+        <li>Message 2</li>
+        <li>Message 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MessageLog
